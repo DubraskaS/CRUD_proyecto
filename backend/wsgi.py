8d03fc61d5889307781343d894fdc26b0dae0602
dubraska <solorzano.dubraska@gmail.com>
2025-10-20 01:22:45 -0400

    corrigiendo error de conexion con flask en vercel

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,6 +7,4 @@ import os
 # Esto permite que 'from app import app' funcione correctamente.
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import app as application 
-# Vercel necesita que la instancia de Flask se llame 'application'
-# Importamos 'app' desde app.py y la renombramos como 'application'
+from app import app 
