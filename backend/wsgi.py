16e132b21796a996cfd6ca7630f33d8fdb2109d8
dubraska <solorzano.dubraska@gmail.com>
2025-10-20 01:19:52 -0400

    corrigiendo para usar vercel serverless (API desde vercel)

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,12 @@
 # backend/wsgi.py
 
+import sys
+import os
+
+# Agrega la ruta de la carpeta 'backend' al path de Python.
+# Esto permite que 'from app import app' funcione correctamente.
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import app as application 
 # Vercel necesita que la instancia de Flask se llame 'application'
 # Importamos 'app' desde app.py y la renombramos como 'application'
