65bf39ef94e3954d3c45843e41d60a840d375958
dubraska <solorzano.dubraska@gmail.com>
2025-10-20 02:30:33 -0400

    corrigiendo error de json x2 para conexion vercel

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,11 +1,11 @@
 # backend/wsgi.py
 
-#import sys
-#import os
+import sys
+import os
 
 # Agrega la ruta de la carpeta 'backend' al path de Python.
 # Esto permite que 'from app import app' funcione correctamente.
-#sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(__file__))
 
 from app import app
 
